e16ddac5d95021a19236c992b2860758e3d0a6e4
wrule <1982775886@qq.com>
2021-06-13 18:49:08 +0800

    基本类型

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 
 console.log('你好，世界');
+
+import { Boolean } from './boolean';
+
+const a = new Boolean('sss');
+
---
+++ src/string.ts
@@ -0,0 +1,35 @@
+import { IStruct } from './struct';
+import { EType } from './type';
+
+export class String implements IStruct {
+  public readonly Name: string;
+
+  public readonly Type: EType;
+
+  public Equal(struct: IStruct): boolean {
+    return this.Type === struct.Type;
+  }
+
+  public Compare(struct: IStruct): number {
+    return this.Type === struct.Type ? 1 : 0;
+  }
+
+  public Contain(struct: IStruct): boolean {
+    return this.Type === struct.Type;
+  }
+
+  public Merge(struct: IStruct): IStruct {
+    return { } as any;
+  }
+
+  public Diff(struct: IStruct): any[] {
+    return [];
+  }
+
+  public constructor(
+    name: string,
+  ) {
+    this.Name = name;
+    this.Type = EType.String;
+  }
+}
