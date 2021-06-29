6dbac50612928144bd40607779fa2471b04be13f
jimao <jimao@jimaodeMacBook-Pro.local>
2021-06-29 13:59:17 +0800

    unknow的方法

---
+++ src/unknow.ts
@@ -0,0 +1,44 @@
+import { IField } from './field';
+import { EType } from './type';
+import { StringHash } from './utils/stringHash';
+
+export class UnknowField implements IField {
+  public constructor(
+    name: string,
+  ) {
+    this.Name = name;
+    this.Type = EType.Unknow;
+  }
+
+  public readonly Name: string;
+
+  public readonly Type: EType;
+
+  public Hash() {
+    return StringHash(this.Type);
+  }
+
+  public Equal(field: IField): boolean {
+    return field.Type === EType.Unknow;
+  }
+
+  public Compare(field: IField): number {
+    return field.Type === EType.Unknow ? 1 : 0;
+  }
+
+  public Contain(field: IField): boolean {
+    return true;
+  }
+
+  public Merge(field: IField): IField {
+    if (field.Type === EType.Unknow) {
+      return this;
+    } else {
+      return field;
+    }
+  }
+
+  public Diff(field: IField): any[] {
+    return [];
+  }
+}
