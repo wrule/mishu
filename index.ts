b968a7e6333df1ccca29ac833abda7fdfad5c14c
wrule <1982775886@qq.com>
2021-07-04 00:59:18 +0800

    测试

--- index.ts
+++ index.ts
@@ -1,2 +1,24 @@
+import { JsFactory } from "./js/jsFactory";
 
 console.log('你好，世界');
+
+const obj = {
+  "name": "jimao",
+  "sex": true,
+  "age": 99,
+  "address": "浙江省杭州市",
+  "tags": ["程序员", "跑步", "看书"],
+  "tuple": [
+    ["m1", 1],
+    ["m2", 2],
+    ["m3", 3]
+  ],
+  "meta": {
+    "color": "red",
+    "lang": ["js", "ts", "c/c++", "go", "c#"]
+  },
+  "unknow": []
+};
+
+const jsField = JsFactory.Create('my', obj);
+console.log(jsField);
