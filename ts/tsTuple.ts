51f1f85096e85aa26b986d6b9712eab1ff3f912a
wrule <1982775886@qq.com>
2021-07-04 16:06:14 +0800

    tuple

--- ts/tsTuple.ts
+++ ts/tsTuple.ts
@@ -2,6 +2,8 @@
 import { TupleField } from '../proto/tuple';
 import { EType } from '../type';
 import { TsField } from './tsField';
+import { TsUndefined } from './tsUndefined';
+import { TsUnion } from './tsUnion';
 
 export class TsTuple extends TupleField implements TsField {
   constructor(
@@ -18,7 +20,18 @@ export class TsTuple extends TupleField implements TsField {
   public Compare(tsField: TsField): number {
     if (tsField.Type === EType.Tuple) {
       const tupleField = tsField as TsTuple;
-      return 1;
+      const maxLength = this.Elements.length > tupleField.Elements.length ?
+        this.Elements.length :
+        tupleField.Elements.length;
+      let sum = 0;
+      for (let i = 0; i < maxLength; ++i) {
+        const field1 = this.Elements[i];
+        const field2 = tupleField.Elements[i];
+        if (field1 && field2) {
+          sum += field2.Compare(field2);
+        }
+      }
+      return sum / (maxLength || 1);
     } else {
       return 0;
     }
@@ -38,7 +51,28 @@ export class TsTuple extends TupleField implements TsField {
     }
   }
 
-  public Merge(tsField: TsField) {
-    return { } as any;
+  public Merge(tsField: TsField): TsField {
+    if (tsField.Type === EType.Tuple) {
+      const tupleField = tsField as TsTuple
+      const similarity = this.Compare(tupleField);
+      if (similarity >= 0.2) {
+        const maxLength = this.Elements.length > tupleField.Elements.length ?
+          this.Elements.length :
+          tupleField.Elements.length;
+        const newElements: TsField[] = [];
+        for (let i = 0; i < maxLength; ++i) {
+          const field1 = this.Elements[i] || new TsUndefined(`element${i}`);
+          const field2 = tupleField.Elements[i] || new TsUndefined(`element${i}`);
+          newElements.push(field1.Merge(field2));
+        }
+        return new TsTuple(this.Name, newElements);
+      } else {
+        return new TsUnion(this.Name, [this, tupleField]);
+      }
+    } else if (tsField.Type === EType.Union) {
+      return tsField.Merge(this);
+    } else {
+      return new TsUnion(this.Name, [this, tsField]);
+    }
   }
 }
