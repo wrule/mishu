6e2ff49293e334a6809a381733d9de7ad722a87b
wrule <1982775886@qq.com>
2021-07-04 17:41:06 +0800

    Contain

--- ts/tsUnion.ts
+++ ts/tsUnion.ts
@@ -1,5 +1,6 @@
 
 import { UnionField } from '../proto/union';
+import { EType } from '../type';
 import { TsField } from './tsField';
 
 export class TsUnion extends UnionField implements TsField {
@@ -12,6 +13,27 @@ export class TsUnion extends UnionField implements TsField {
 
   public get Members() {
     return this.members as TsField[];
+  }
+
+  public Compare(tsField: TsField): number {
+    if (tsField.Type === EType.Union) {
+
+    } else {
+      return 0;
+    }
+  }
+
+  public Contain(tsField: TsField): boolean {
+    if (tsField.Type === EType.Union) {
+      const unionField = tsField as TsUnion;
+      return unionField.Members.every(
+        (member) => this.Contain(member)
+      );
+    } else {
+      return this.Members.some(
+        (member) => member.Contain(tsField)
+      );
+    }
   }
 
   public Merge(tsField: TsField) {
