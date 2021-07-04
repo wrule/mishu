e23e2b1236a0436b402e6dccf8df832d0226e051
wrule <1982775886@qq.com>
2021-07-04 12:49:28 +0800

    tuple

--- ts/tsArray.ts
+++ ts/tsArray.ts
@@ -1,6 +1,8 @@
 
 import { ArrayField } from '../proto/array';
+import { EType } from '../type';
 import { TsField } from './tsField';
+import { TsUnion } from './tsUnion';
 
 export class TsArray extends ArrayField implements TsField {
   constructor(
@@ -14,7 +16,32 @@ export class TsArray extends ArrayField implements TsField {
     return this.element as TsField;
   }
 
-  public Merge(tsField: TsField) {
-    return { } as any;
+  public Compare(tsField: TsField): number {
+    if (tsField.Type === EType.Array) {
+      const arrayField = tsField as TsArray;
+      return (this.Element.Compare(arrayField.Element) * 0.9) + 0.1;
+    } else {
+      return 0;
+    }
+  }
+
+  public Contain(tsField: TsField): boolean {
+    if (tsField.Type === EType.Array) {
+      const arrayField = tsField as TsArray;
+      return this.Element.Contain(arrayField.Element);
+    } else {
+      return false;
+    }
+  }
+
+  public Merge(tsField: TsField): TsField {
+    if (tsField.Type === EType.Array) {
+      const arrayField = tsField as TsArray;
+      return new TsArray(this.Name, this.Element.Merge(arrayField.Element));
+    } else if (tsField.Type === EType.Union) {
+      return tsField.Merge(this);
+    } else {
+      return new TsUnion(this.Name, [this, tsField]);
+    }
   }
 }
--- ts/tsTuple.ts
+++ ts/tsTuple.ts
@@ -1,5 +1,6 @@
 
 import { TupleField } from '../proto/tuple';
+import { EType } from '../type';
 import { TsField } from './tsField';
 
 export class TsTuple extends TupleField implements TsField {
@@ -12,6 +13,29 @@ export class TsTuple extends TupleField implements TsField {
 
   public get Elements() {
     return this.elements as TsField[];
+  }
+
+  public Compare(tsField: TsField): number {
+    if (tsField.Type === EType.Tuple) {
+      const tupleField = tsField as TsTuple;
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  public Contain(tsField: TsField): boolean {
+    if (tsField.Type === EType.Tuple) {
+      const tupleField = tsField as TsTuple;
+      return (
+        this.Elements.length === tupleField.Elements.length &&
+        this.Elements.every(
+          (element, index) => element.Contain(tupleField.Elements[index])
+        )
+      );
+    } else {
+      return false;
+    }
   }
 
   public Merge(tsField: TsField) {
