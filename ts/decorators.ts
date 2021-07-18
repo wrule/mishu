7b673aacf8dfd7ea8698c4bf13905ec1e4a44d84
jimao <jimao@jimaodeMBP.lan>
2021-07-18 12:14:06 +0800

    xlsx

--- ts/decorators.ts
+++ ts/decorators.ts
@@ -1,6 +1,26 @@
 import { EType } from '../type';
 import { TsField } from './tsField';
 
+export function BeforeContain() {
+  return function (
+    target: any,
+    key: string | symbol,
+    descriptor: PropertyDescriptor,
+  ) {
+    const original = descriptor.value;
+    descriptor.value = function(...args: any[]) {
+      const that = this as TsField;
+      const tsField = args[0] as TsField;
+      if (tsField.Hash() === that.Hash()) {
+        return true;
+      }
+      const result = original.apply(this, args);
+      return result;
+    };
+    return descriptor;
+  };
+}
+
 export function BeforeCompare() {
   return function (
     target: any,
