f5534e9fb66268605f1e4302bcadcd51b333852d
wrule <1982775886@qq.com>
2021-07-20 23:26:48 +0800

    注释

--- src/ts/modelCode.ts
+++ src/ts/modelCode.ts
@@ -1,12 +1,24 @@
-
+/**
+ * TypeScript定义代码模型
+ */
 export class ModelCode {
   constructor(
     private name: string,
     private define: string = '',
   ) { }
 
+  /**
+   * 类型名称
+   */
   public get Name() {
     return this.name;
+  }
+
+  /**
+   * 类型定义代码
+   */
+  public get Define() {
+    return this.define;
   }
 
   public get ModuleName() {
@@ -15,9 +27,5 @@ export class ModelCode {
 
   public get InterfaceName() {
     return this.Name;
-  }
-
-  public get Define() {
-    return this.define;
   }
 }
