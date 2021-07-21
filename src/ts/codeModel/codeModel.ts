d237238b93004cd2132c732be0eebc165f19f006
wrule <1982775886@qq.com>
2021-07-21 21:53:35 +0800

    添加parent

--- src/ts/codeModel/codeModel.ts
+++ src/ts/codeModel/codeModel.ts
@@ -3,10 +3,15 @@ import { TsField } from '../tsField';
 export abstract class CodeModel {
   constructor(
     protected tsField: TsField,
+    protected parent?: CodeModel,
   ) { }
 
   public get Name() {
     return this.tsField.Name;
+  }
+
+  public get Parent() {
+    return this.parent;
   }
 
   abstract InterfaceName: string;
