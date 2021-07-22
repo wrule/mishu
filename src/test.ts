1f12cfc066cd5f5ac046a6fa8f196af1bbe9cafc
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-22 10:32:22 +0800

    修改测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,34 +1,38 @@
 import fs from 'fs';
 import { TS, TSCode } from './index';
 
-// const jsonObject = {
-//   "name": "jimao",
-//   "sex": true,
-//   "age": 99,
-//   "address": "浙江省杭州市",
-//   "tags": ["程序员", "跑步", "看书"],
-//   "tuple": [
-//     ["m1", 1],
-//     ["m2", 2],
-//     ["m3", 3]
-//   ],
-//   "meta": {
-//     "color": "red",
-//     "lang": ["js", "ts", "c/c++", "go", "c#", 12, true, 12, 12, 12]
-//   },
-//   "korean": {
-//     "movie": 13,
-//     "name": "korean",
-//     "nini": null,
-//     "bj": {
-//       "roomId": "sjsjjdhd",
-//       "fansNum": 12228277,
-//       "test": ["112", true]
-//     }
-//   },
-//   "unknow": []
-// };
+const jsonObject = {
+  "list": [
+    { "name": "sdf", "age": 14 },
+    { "name": "wrule", "age": 72 }
+  ],
+  "name": "jimao",
+  "sex": true,
+  "age": 99,
+  "address": "浙江省杭州市",
+  "tags": ["程序员", "跑步", "看书"],
+  "tuple": [
+    ["m1", 1],
+    ["m2", 2],
+    ["m3", 3]
+  ],
+  "meta": {
+    "color": "red",
+    "lang": ["js", "ts", "c/c++", "go", "c#", 12, true, 12, 12, 12]
+  },
+  "korean": {
+    "movie": 13,
+    "name": "korean",
+    "nini": null,
+    "bj": {
+      "roomId": "sjsjjdhd",
+      "fansNum": 12228277,
+      "test": ["112", true]
+    }
+  },
+  "unknow": []
+};
 
-const modelCode = TS('').Merge(TS(1)).ToCodeModel();
+const modelCode = TSCode(jsonObject, 'rsp');
 console.log(modelCode.InterfaceNameInContext);
 fs.writeFileSync('output/output.ts', modelCode.DefineCode);
